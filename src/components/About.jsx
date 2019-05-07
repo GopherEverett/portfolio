92095bdaf15284be516f224eec26f9ec06c0fa45
John Jacobs <john.a.jacobs1973@gmail.com>
2019-05-07 10:44:19 -0500

    changed photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 const AboutDiv = styled.div`
     text-align: center;
-
+    p {
+        font-size: 1.5rem;
+    }
 `
 export default class About extends Component {
   render() {
@@ -10,10 +12,12 @@ export default class About extends Component {
       <AboutDiv>
       <br/>
         <h2>Full-Stack Developer</h2>
-        <p>Atlanta, GA</p>
+        <h3>Atlanta, GA</h3>
+        <br/>
         <p>I am a full-stack developer who is driven by a challenge.</p>
-        <p>Professionally and personally my focus is on progress.</p>
-        <p>Whether coding, running, or riding I am always moving forward.</p>
+        <p>I have a passion for physical fitness, and bring the same commitment and energy to any project I am involved with.</p>
+        <p>Professionally and personally my focus is on progress;</p>
+        <p>whether coding, running, or riding I am always moving forward.</p>
       </AboutDiv>
     )
   }

63020143ed8db33b6df8ede2b6e1ccd594d5900b
John Jacobs <john.a.jacobs1973@gmail.com>
2019-05-07 09:31:39 -0500

    final revisions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,12 @@ export default class About extends Component {
   render() {
     return (
       <AboutDiv>
+      <br/>
         <h2>Full-Stack Developer</h2>
         <p>Atlanta, GA</p>
-        <p>I am driven by a challenge</p>
-        <p>Whether coding, running, or riding I am always moving forward</p>
+        <p>I am a full-stack developer who is driven by a challenge.</p>
+        <p>Professionally and personally my focus is on progress.</p>
+        <p>Whether coding, running, or riding I am always moving forward.</p>
       </AboutDiv>
     )
   }
